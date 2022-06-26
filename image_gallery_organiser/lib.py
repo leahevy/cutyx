bf5a447615c835681cb27bbd87c2eb154f3d6071
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-26 18:34:50 +0200

    Implement add person; minor changes

--- image_gallery_organiser/lib.py
+++ image_gallery_organiser/lib.py
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import hashlib
 import json
 import os
 import os.path
@@ -50,8 +51,9 @@ def process_directory(
     symlink: bool = True,
     use_cache: bool = True,
     quiet: bool = False,
+    only_process_files: list[str] | None = None,
 ) -> None:
-    raise NotImplementedError()
+    raise NotImplementedError("process_directory")
 
 
 def process_image(
@@ -63,38 +65,110 @@ def process_image(
     use_cache: bool = True,
     quiet: bool = False,
 ) -> None:
-    raise NotImplementedError()
+    check_valid_image(image_to_process_path)
+    dirname = os.path.dirname(image_to_process_path)
+    process_directory(
+        dirname,
+        albums_root_dir=albums_root_dir,
+        dry_run=dry_run,
+        delete_old=delete_old,
+        symlink=symlink,
+        use_cache=use_cache,
+        quiet=quiet,
+        only_process_files=[image_to_process_path],
+    )
+
+
+FACES_DIR_NAME = ".faces.d"
+
+
+def handle_dry_run(dry_run: bool) -> None:
+    if dry_run:
+        print("[blue]++ DRY RUN ++[/blue]")
 
 
 def add_persons(
-    training_image_path: str,
     album_dir: str,
+    training_image_path: str,
     dry_run: bool = False,
     training_data_prefix: str | None = None,
     quiet: bool = False,
 ) -> None:
-    raise NotImplementedError()
+    handle_dry_run(dry_run)
+    check_valid_image(training_image_path)
+
+    if not quiet:
+        print(
+            f"[green]++ Calculate face encodings '{training_image_path}' ++[/green]"
+        )
     encodings = get_face_encodings(training_image_path)
+
     if len(encodings) == 0:
-        raise ValueError("No face recognised in image")
-    elif len(encodings) > 1:
-        raise ValueError(
-            "Multiple faces recognised. Requires image with single face."
+        raise FacesException(
+            f"No face recognised in image '{training_image_path}."
         )
 
-    serialized_as_json = json.dumps(
-        pickle.dumps(encodings[0]).decode("latin-1")
-    )
+    with open(training_image_path, "rb") as f:
+        image_hash = hashlib.md5(f.read()).hexdigest()
 
-    output_dir = os.path.join(album_dir, ".faces.d")
+    output_training_dir_name = image_hash + ".d"
+    if training_data_prefix:
+        output_training_dir_name = (
+            training_data_prefix + "-" + output_training_dir_name
+        )
+
+    output_dir = os.path.join(
+        album_dir, FACES_DIR_NAME, output_training_dir_name
+    )
+    if not quiet:
+        print(
+            f"[green]++ Create training data directory '{output_dir}' ++[/green]"
+        )
+    try:
+        shutil.rmtree(output_dir)
+    except FileNotFoundError:
+        pass
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-    import hashlib
+    for idx, encoding in enumerate(encodings):
+        if not quiet:
+            print(
+                f"[green]++ Writing face encoding {idx + 1}/{len(encodings)} ++[/green]"
+            )
+        serialized_as_json = json.dumps(
+            pickle.dumps(encoding).decode("latin-1")
+        )
+        md5 = str(hashlib.md5(serialized_as_json.encode("utf-8")).hexdigest())
+        output_file = os.path.join(output_dir, md5 + ".encoding")
+        if not dry_run:
+            with open(output_file, "w") as f:
+                f.write(serialized_as_json)
+    symlink_path = image_hash + ".src"
+    if training_data_prefix:
+        symlink_path = training_data_prefix + "-" + symlink_path
+    symlink_path = os.path.join(album_dir, FACES_DIR_NAME, symlink_path)
+    if not quiet:
+        print("[green]++ Create symlink to training file ++[/green]")
+    if not dry_run:
+        try:
+            os.remove(symlink_path)
+        except FileNotFoundError:
+            pass
+        os.symlink(os.path.abspath(training_image_path), symlink_path)
 
-    md5 = str(hashlib.md5(serialized_as_json.encode("utf-8")).hexdigest())
-    output_file = os.path.join(output_dir, md5 + ".encoding")
-    with open(output_file, "w") as f:
-        f.write(serialized_as_json)
+
+def check_valid_image(image_path: str) -> None:
+    if not os.path.exists(image_path):
+        raise FacesException(f"Path '{image_path}' does not exist.")
+    if not os.path.isfile(image_path):
+        raise FacesException(f"Path '{image_path}' is no file.")
+    if not (
+        image_path.lower().endswith(".jpg")
+        or image_path.lower().endswith(".jpeg")
+    ):
+        raise FacesException(
+            f"File '{image_path}' has an invalid file type (only JPEGs are supported)."
+        )
 
 
 def get_face_encodings(image_path: str) -> Any:
@@ -116,7 +190,7 @@ def get_face_encodings(image_path: str) -> Any:
 
 
 def person_matches(image_path: str) -> None:
-    raise NotImplementedError()
+    raise NotImplementedError("person_matches")
     from image_gallery_organiser import faces
 
     encodings = get_face_encodings(image_path)
